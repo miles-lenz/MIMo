1b58bd03bf5258c1682066f33a81169bf7838294
miles-lenz <miles@lenz-be.de>
2025-01-14 11:36:35 +0100

    add possibilty to switch between CSA and volume for gear prediction

--- mimoGrowth/mujoco/motor.py
+++ mimoGrowth/mujoco/motor.py
@@ -5,7 +5,7 @@ from mujoco import MjModel
 import numpy as np
 
 
-def calc_motor_gear(geoms: dict, model_og: MjModel) -> dict:
+def calc_motor_gear(geoms: dict, model_og: MjModel, use_csa: bool = True) -> dict:
     """..."""
 
     # Iterate over geoms and their corresponding motors.
@@ -16,14 +16,21 @@ def calc_motor_gear(geoms: dict, model_og: MjModel) -> dict:
         size = geoms[geom]["size"]
         size_og = model_og.geom_size[model_og.geom(geom).id]
 
-        # Compute the CSA with the current and the original size.
-        # The formular will change depending on the type of geom.
-        if geom in ["geom:right_hand1", "geom:right_foot2"]:  # box
-            csa = size[0] * size[1]
-            csa_og = size_og[0] * size_og[1]
-        else:  # sphere/capsule
-            csa = np.pi * size[0] ** 2
-            csa_og = np.pi * size_og[0] ** 2
+        # Get the type of the geom.
+        type_ = model_og.geom_type[model_og.geom(geom).id]
+
+        # Calculate the CSA and volume with the current and original size.
+        # The formula will change depending on the type of geom.
+        if type_ in [2, 3]:  # sphere/capsule
+            csa, csa_og = np.pi * size[0] ** 2, np.pi * size_og[0] ** 2
+            vol, vol_og = (4 / 3) * np.pi * size[0] ** 3, (4 / 3) * np.pi * size_og[0] ** 3
+            if type_ == 3:  # capsule:
+                vol += np.pi * size[0] ** 2 * size[1] * 2
+                vol_og += np.pi * size_og[0] ** 2 * size_og[1] * 2
+
+        elif type_ == 6:  # box
+            csa, csa_og = size[0] * size[1] * 4, size_og[0] * size_og[1] * 4
+            vol, vol_og = np.prod(size) * 8, np.prod(size_og) * 8
 
         # Calculate the gear value for each motor.
         for motor in motors:
@@ -34,11 +41,14 @@ def calc_motor_gear(geoms: dict, model_og: MjModel) -> dict:
             except KeyError:
                 continue
 
-            # Calculate the ratio based on the original gear and CSA values.
-            ratio = gear_og / csa_og
-
-            # Calculate the gear value for the current age based on the CSA value and the ratio.
-            gear = csa * ratio
+            # Calculate the ratio based on the original gear and CSA/volume values.
+            # Then, calculate the gear value for the current age based on the CSA/volume value and the ratio.
+            if use_csa:
+                ratio = gear_og / csa_og
+                gear = csa * ratio
+            else:
+                ratio = gear_og / vol_og
+                gear = vol * ratio
 
             # Store the gear value. If the motor is specifically for a 'right' body part,
             # store it also for its 'left' counterpart.
